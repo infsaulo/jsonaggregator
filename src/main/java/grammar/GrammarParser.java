197617969c79ec0be66edc435d2c3511fe0e2146
Saulo Ricci <infsaulok@gmail.com>
2016-08-11 09:00:40 -0300

    Adding the gradle structure

--- src/main/java/grammar/GrammarParser.java
+++ src/main/java/grammar/GrammarParser.java
@@ -26,8 +26,8 @@ public class GrammarParser {
     Preconditions.checkState(fields.size() > 0, "fields list must contain at least one element");
 
     for (final Field field : fields) {
+
       validateRequiredFields(field);
-      validateField(field);
     }
   }
 
@@ -35,94 +35,7 @@ public class GrammarParser {
       throws NullPointerException, IllegalStateException {
 
     Preconditions.checkNotNull(field.name, "name field must be present");
-    Preconditions.checkNotNull(field.destName, "destName field must be present");
-    Preconditions.checkNotNull(field.srcType, "srcType field must be present");
-    Preconditions.checkNotNull(field.bqType, "bqType field must be present");
-
-    if (field.srcType.equals("RECORD")) {
-      Preconditions.checkState(field.getFields().size() > 0,
-          "fields inside a record must contain at least one element");
-      for (final Field innerField : field.getFields()) {
-        validateRequiredFields(innerField);
-        validateField(innerField);
-      }
-    }
-  }
-
-  private static void validateField(final Field field) throws IllegalStateException {
-
-    validateTypeMapping(field);
-  }
-
-  private static void validateIntegerMapping(final Field field) throws IllegalStateException {
-
-    switch (field.getBqType()) {
-
-      case "STRING":
-        break;
-      case "INTEGER":
-        break;
-      case "FLOAT":
-        break;
-      default:
-        throw new IllegalStateException(field.getBqType() + " cannot be type casted from INTEGER");
-    }
-
-  }
-
-  private static void validateDecimalMapping(final Field field) throws IllegalStateException {
-
-    switch (field.getBqType()) {
-
-      case "STRING":
-        break;
-      case "FLOAT":
-        break;
-      default:
-        throw new IllegalStateException(field.getBqType() + " cannot be type casted from DECIMAL");
-    }
-  }
-
-  private static void validateStringMapping(final Field field) throws IllegalStateException {
-
-    switch (field.getBqType()) {
-
-      case "STRING":
-        break;
-      case "BYTES":
-        break;
-      default:
-        throw new IllegalStateException(field.getBqType() + " cannot be type casted from STRING");
-    }
-  }
-
-  private static void validateRecordMapping(final Field field) throws IllegalStateException {
-    switch (field.getBqType()) {
-
-      case "RECORD":
-        break;
-      default:
-        throw new IllegalStateException(field.getBqType() + " cannot be type casted from RECORD");
-    }
-  }
-
-  private static void validateTypeMapping(final Field field) throws IllegalStateException {
-
-    switch (field.getSrcType()) {
-      case "INTEGER":
-        validateIntegerMapping(field);
-        break;
-      case "DECIMAL":
-        validateDecimalMapping(field);
-        break;
-      case "STRING":
-        validateStringMapping(field);
-        break;
-      case "RECORD":
-        validateRecordMapping(field);
-        break;
-      default:
-        throw new IllegalStateException(field.getSrcType() + " is not supported as a source type");
-    }
+    Preconditions.checkNotNull(field.directions, "directions field must be present");
+    Preconditions.checkNotNull(field.srcName, "srcName field must be present");
   }
 }
